e60083818e9c3678dda44f6d18a98c10632ca9f4
JGageWright <83935187+JGageWright@users.noreply.github.com>
2021-05-12 16:55:42 -0500

    Organize supporting functions
    Steps toward generalization

--- importer_snippets.py
+++ importer_snippets.py
@@ -1,20 +1,45 @@
 import pandas as pd
 import glob, os, time, openpyxl
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
+import tkinter as tk
 
-#SSM Importer
-def get_filenames(folder_path):
-    """
+def get_folders(folder_list=None):
+    '''
+    Gets path of any set of directories from the user manually
+    '''
+    if folder_list == None:
+        folder_list = []
+    folder_path = filedialog.askdirectory(title='Select Data Folder')
+    folder_list.append(folder_path)
+    prompt = tk.messagebox.askyesno('Data Folder Selection',
+                                    'Are there any other folders?', icon='warning')
+    if prompt == True:
+        get_folders(folder_list)
+
+    return folder_list
+
+
+def get_filenames_from_folder(folder_path, extension=None):
+    '''
     Takes folder_path
     Returns list of .SSM file paths in folder_path
-    """
+    '''
     filenames = []
-    for file in glob.glob(folder_path + '/*.ssm'):
+    for file in glob.glob(folder_path + '/*' + str(extension)):
         filenames.append(file)
     filenames.sort()  # Perhaps unnecessary, brings me peace
     return filenames
 
+def get_filename_and_extension(path):
+    '''
+    Previously get_column_name
+    Takes file path
+    Returns filename and extension only for column naming
+    '''
+    return path.split("\\")[-1]
 
+
+#Stellarnet/Spectrawiz SSM Importer
 def series_stellarnet_ssm(file):
     """
     Takes path to .SSM file and imports using pandas from_csv() to a Series
@@ -37,16 +62,6 @@ def concat_me(DataFrame, Series, name=None):
     return pd.concat([DataFrame, Series], axis=1)
 
 
-def get_column_name(file):
-    """
-    Takes file, which is a path
-    Returns filename and extension only for column naming
-    """
-    split_list = file.split("\\")
-    last_element = split_list[-1]
-    return last_element
-
-
 def import_ssm_folder(folder_path):
     """
     Takes folder path, abstracts to get filenames list and
@@ -55,14 +70,14 @@ def import_ssm_folder(folder_path):
 
     Dependent on tkinter, glob, pandas
     """
-    filenames = get_filenames(folder_path)
+    filenames = get_filenames_from_folder(folder_path, '.ssm')
     basis = pd.read_csv(filenames[0],
                         skiprows=1,
                         delim_whitespace=True,
-                        names=['Wavelength', get_column_name(filenames[0])])
+                        names=['Wavelength', get_filename_and_extension(filenames[0])])
     for file in filenames[1::1]:
         series = series_stellarnet_ssm(file)
-        name = get_column_name(file)
+        name = get_filename_and_extension(file)
         basis = concat_me(basis, series, name)
     basis.set_index(basis.columns[0], inplace=True)
     return basis
@@ -84,14 +99,14 @@ def get_params(folder_path):
     """
     Grabs the parameters reported by the stellarnet system
     """
-    filenames = get_filenames(folder_path)
+    filenames = get_filenames_from_folder(folder_path, '.ssm')
     head = pd.read_csv(filenames[0], nrows=0, delim_whitespace=True)
     params = list(head.columns.values)
     return params
 
 
 #ELISA Importer
-def make_dataframe(lines):
+def make_ELISA_dataframe(lines):
     '''
     Takes sorted sets lines from the raw text file
     Places OD data in units of (a.u.) in dataframes that maintain the 8x12 shape (but inlcude indexes).
@@ -124,11 +139,11 @@ def make_dataframe(lines):
 #     n = int((len(lines)) / 11)
 #     output_dataframes = []
 #     for i in range(n):
-#         df = make_dataframe(lines[11*i:11*i + 8])
+#         df = make_ELISA_dataframe(lines[11*i:11*i + 8])
 #         output_dataframes.append(df)
 
 
-#Episodic Importer
+#Episodic Importers
 def episodic_to_dataframe(file):
     '''
     Takes .EP1x file, returns dataframe indexed by wavelength and 'epx'
@@ -148,10 +163,10 @@ def episodic_to_dataframe(file):
     return raw
 
 
-def export_files(df_list, params):
+def export_dfl(df_list, params):
     '''
-    Takes the list of [filename, df] pairs generated from the data folder
-    Saves each df and params to separate sheets in a single .xlsx file in the save location
+    Takes the list of [filename, df] pairs generated from legacy FRET script
+    Saves each dataframe (and input params) to separate sheets in a single .xlsx file in the save location
     '''
     save_path = filedialog.asksaveasfile(title='Select Save Location',
                                          filetypes=[('Microsoft Excel Worksheet', '.xlsx')])
@@ -167,6 +182,27 @@ def export_files(df_list, params):
         writer.close()
 
 
+def export_dfl_list(dfl_list, params):
+    '''
+    Takes the dfl_list generated from the data folder
+    Saves each signal df and params to separate sheets in a single .xlsx file in the save location
+
+    Recall that dfl_list is a monster object of many nested lists, which during its invention indexed like:
+    dfl_list[conc_1, ...][trial_1, ... , trial_n, signal][params, spectra_dataframe]
+
+    OPEN ISSUE: the params that is passed in does not necessarily apply to each set of spectra.
+    '''
+    save_path = filedialog.asksaveasfile(title='Select Save Location',
+                                         filetypes=[('Microsoft Excel Worksheet', '.xlsx')])
+    save_name = save_path.name
+    if save_name[-5:] != '.xlsx':
+        save_name = str(save_name + '.xlsx')
+    with pd.ExcelWriter(save_name) as writer:
+        for i in range(len(dfl_list)):
+            dfl_list[i][-1].to_excel(writer, sheet_name='Conc' + str(i))
+        params.to_excel(writer, sheet_name='params')
+
+#SET OFFSET HERE IN SECONDS
 def get_times(params, episodes):
     '''
     Returns a list of times, marking the start of each collection period
@@ -177,7 +213,7 @@ def get_times(params, episodes):
     times = []
     for ep in range(episodes):
         times.append(
-            int(ep * (collection_period + delay))
+            int(ep * (collection_period + delay) + 15) #SET OFFSET HERE IN SECONDS
         )
     return times
 
