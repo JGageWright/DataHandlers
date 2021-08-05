f9b03b47a93dd119a4ca312af715834415832d29
JGageWright <83935187+JGageWright@users.noreply.github.com>
2021-08-05 13:56:50 -0500

    Finish FTIR updates

--- notebooks/FTIR_JGW-A-53.py
+++ notebooks/FTIR_JGW-A-53.py
@@ -18,7 +18,7 @@ DAD_df = importer_snippets.cary630(r'..\data\JGW-A-53\1,10-dad_jgw-a-53-19.csv')
 DADD_df = importer_snippets.cary630(r'..\data\JGW-A-53\1,12-dadd_jgw-a-53-24.csv')
 
 # Subtract the heneicosane/urea spectrum
-subtract = True
+subtract = False
 
 if subtract == True:
     DAB_df['Absorbance'] = DAB_df['Absorbance'] - Hen_df['Absorbance']
@@ -45,30 +45,43 @@ for ax in all_ax:
     ax.set_xlim(1760, 1700)
     ax.set_ylim(0, 2)
 
-fig, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplots(6, 1, sharex='all', sharey='all')
-fig.set_size_inches(4, 40)
+fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, sharex='all', sharey='all')
+# fig.set_size_inches(8, 11)
+
 
 ax1.plot(Hen_df['Wavenumber'], DAB_df['Absorbance'])
-ax1.set_title('1,4-diacetoxybutane/urea')
+ax1.set_title('1,4-diacetoxybutane/urea', size=12)
+ax1.annotate('JGW-A-53-34', (.7, .9), xycoords=ax1.transAxes, size='10')
 
 ax2.plot(Hen_df['Wavenumber'], DAH_df['Absorbance'])
-ax2.set_title('1,6-diacetoxyhexane/urea')
+ax2.set_title('1,6-diacetoxyhexane/urea', size=12)
+ax2.annotate('JGW-A-53-15', (.7, .9), xycoords=ax2.transAxes, size='10')
 
 ax3.plot(Hen_df['Wavenumber'], DAO_df['Absorbance'])
-ax3.set_title('1,8-diacetoxyoctane/urea')
+ax3.set_title('1,8-diacetoxyoctane/urea', size=12)
+ax3.annotate('JGW-A-53-32', (.7, .9), xycoords=ax3.transAxes, size='10')
+
 
 ax4.plot(Hen_df['Wavenumber'], DAN_df['Absorbance'])
-ax4.set_title('1,9-diacetoxynonane/urea')
+ax4.set_title('1,9-diacetoxynonane/urea', size=12)
+ax4.annotate('JGW-A-53-30', (.7, .9), xycoords=ax4.transAxes, size='10')
 
 ax5.plot(Hen_df['Wavenumber'], DAD_df['Absorbance'])
-ax5.set_title('1,10-diacetoxydecane/urea')
+ax5.set_title('1,10-diacetoxydecane/urea', size=12)
+ax5.annotate('JGW-A-53-19', (.7, .9), xycoords=ax5.transAxes, size='10')
 
 ax6.plot(Hen_df['Wavenumber'], DADD_df['Absorbance'])
-ax6.set_title('1,12-diacetoxydodecane/urea')
+ax6.set_title('1,12-diacetoxydodecane/urea', size=12)
+ax6.annotate('JGW-A-53-24', (.7, .9), xycoords=ax6.transAxes, size='10')
 
 ax1.set_xlim(1760, 1700)
 ax1.set_ylim(0, 2)
 
-plt.subplots_adjust(wspace=10)
+
+
+fig.suptitle('Unsubtracted', size=24)
+fig.supylabel('Wavenumber (cm$^{-1}$)', size=16)
+fig.supxlabel('Absorbance (a.u.)', size=16)
+plt.tight_layout()
 
 plt.show()
