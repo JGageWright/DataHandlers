ec4dff1304d80b42bef7d9cc16145d8e3e194df4
JGageWright <83935187+JGageWright@users.noreply.github.com>
2021-10-04 16:33:54 -0700

    GC update
    Stopped automatic spline plot saving.
    Made graph output smaller for jupyter.

--- .ipynb_checkpoints/color_test-checkpoint.py
+++ .ipynb_checkpoints/color_test-checkpoint.py
@@ -9,7 +9,7 @@ ax0 = fig.add_subplot(111)
 
 ax0.plot(x, np.sin(x + 0 * np.pi/6))
 # ax1 = plt.plot(x, np.sin(x + 1 * np.pi/6))
-ax2 = plt.scatter(x, np.sin(x + 2 * np.pi/6))
+ax2 = plt.plot(x, np.sin(x + 2 * np.pi/6))
 # ax3 = plt.plot(x, np.sin(x + 3 * np.pi/6))
 ax4 = plt.plot(x, np.sin(x + 4 * np.pi/6))
 # ax5 = plt.plot(x, np.sin(x + 5 * np.pi/6))
@@ -20,6 +20,8 @@ ax8 = plt.plot(x, np.sin(x + 8 * np.pi/6))
 ax10 = plt.plot(x, np.sin(x + 10 * np.pi/6))
 # ax11 = plt.plot(x, np.sin(x + 11 * np.pi/6))
 
+plt.grid()
 ax0.set_title('Title')
+
 
 plt.show()
