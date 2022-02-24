9a8f76c6b09a6cbfc9a1a47632fe5621b091e450
JGageWright <83935187+JGageWright@users.noreply.github.com>
2022-02-24 08:09:26 -0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -61,29 +61,116 @@ def CHI_plot(df, technique, option=None, label=None):
         
     return fig, ax
 
-def CHI_add_coplot(df, ax, technique, option=None, label=None):
-    if technique.lower() in ['a.c. impedance', 'imp', 'eis', 'peis']:
-        if option != None and option.lower() == 'bode':
-            ax[0].scatter(np.log10(df['Freq/Hz']), np.log10(df['Zmag/ohm']), label=label)
-            ax[0].set_xlabel('log$_{10}(f)$')
-            ax[0].set_ylabel('log$_{10}|Z|$')
+# def CHI_add_coplot(df, ax, technique, option=None, label=labels[i]=None):
+#     if technique.lower() in ['a.c. impedance', 'imp', 'eis', 'peis']:
+#         if option != None and option.lower() == 'bode':
+#             ax[0].scatter(np.log10(df['Freq/Hz']), np.log10(df['Zmag/ohm']), label=labels[i]=label=labels[i])
+#             ax[0].set_xlabel('log$_{10}(f)$')
+#             ax[0].set_ylabel('log$_{10}|Z|$')
             
-            ax[1].scatter(np.log10(df['Freq/Hz']), -df['Phase/deg'], label=label)
-            ax[1].set_xlabel('log$_{10}(f) $')
-            ax[1].set_ylabel('$-\phi$ / $\degree$')
-        else:
-            ax.plot(df['Zre/ohm'], -df['Zim/ohm'], label=label)
-            ax.set_xlabel('$Z_{real}$ / $\Omega$')
-            ax.set_ylabel('$-Z_{imag}$ / $\Omega$')
+#             ax[1].scatter(np.log10(df['Freq/Hz']), -df['Phase/deg'], label=labels[i]=label=labels[i])
+#             ax[1].set_xlabel('log$_{10}(f) $')
+#             ax[1].set_ylabel('$-\phi$ / $\degree$')
+#         else:
+#             ax.plot(df['Zre/ohm'], -df['Zim/ohm'], label=labels[i]=label=labels[i])
+#             ax.set_xlabel('$Z_{real}$ / $\Omega$')
+#             ax.set_ylabel('$-Z_{imag}$ / $\Omega$')
             
-    elif technique.lower() in ['cyclic voltammetry', 'linear sweep voltammetry', 'cv', 'lsv']:
-        ax.plot(df['Potential/V'], df['Current/A']*1000, label=label)
-        ax.set_xlabel('$E$ vs. RE / V')
-        ax.set_ylabel('$i$ / mA')
+#     elif technique.lower() in ['cyclic voltammetry', 'linear sweep voltammetry', 'cv', 'lsv']:
+#         ax.plot(df['Potential/V'], df['Current/A']*1000, label=labels[i]=label=labels[i])
+#         ax.set_xlabel('$E$ vs. RE / V')
+#         ax.set_ylabel('$i$ / mA')
         
-    elif technique.lower() in ['ocp', 'ocpt', 'open circuit potential - time', 'multi-current steps', 'istep', 'cg']:
-        ax.plot(df['Time/sec'], df['Potential/V'], label=label)
-        ax.set_ylabel('$t$ / s')
-        ax.set_ylabel('$E$ vs. RE / V')
+#     elif technique.lower() in ['ocp', 'ocpt', 'open circuit potential - time', 'multi-current steps', 'istep', 'cg']:
+#         ax.plot(df['Time/sec'], df['Potential/V'], label=labels[i]=label=labels[i])
+#         ax.set_ylabel('$t$ / s')
+#         ax.set_ylabel('$E$ vs. RE / V')
     
-    return ax
+#     return ax
+
+def CHI_coplot(df_list, technique, ax=False, fig=False, option=None, labels=None, order=None):
+    """Generates coplotted data from CHI data
+    
+    Note:
+        if fig, ax are supplied as a set of subplots that does not match the technique (i.e. bode/imp), 
+        pyplot throws error that ax is np.ndarray.
+        
+    Known Bug:
+        list index error if option='bode' and order is not None.
+        Currently, orders for option='bode' are ignored
+
+    Args:
+        df_list (list): list of dataframes from CHI_txt
+        technique (str): Electrochemical technique; used to determine what plot to draw.
+        ax (bool, optional): Existing ax to plot on. Defaults to False.
+        fig (bool, optional): Existing fig to plot on. Defaults to False.
+        option (str, optional): Options for different plots within a technique. Defaults to None.
+        labels (list, optional): Labels for plots. Defaults to None.
+        order (list, optional): Order to draw figure legend with. Defaults to None.
+
+    Raises:
+        ValueError: Both fig and ax or neither fig nor ax must be passed
+
+    Returns:
+        tuple: fig, ax
+    """
+    # Handle option defaults
+    ax_isarray = False
+    if order == None:
+        order = range(len(df_list))
+    if labels == None:
+        labels = []
+        for i in range(len(df_list)):
+            labels.append('_')
+        draw_legend = False
+    else:
+        draw_legend = True
+   
+    if ax is False or fig is False:
+        if fig is not False or ax is not False:
+            raise ValueError('supply CHI_coplot with fig and ax or neither')
+        if option != None and option.lower() == 'bode':
+            fig, ax = plt.subplots(1, 2, figsize=(24, 8))
+        else:
+            fig, ax = plt.subplots()
+            
+    for i, df, in enumerate(df_list):
+        
+        if technique.lower() in ['a.c. impedance', 'imp', 'eis', 'peis']:
+            if option != None and option.lower() == 'bode':
+                ax_isarray = True
+                ax[0].scatter(np.log10(df['Freq/Hz']), np.log10(df['Zmag/ohm']), label=labels[i])
+                ax[0].set_xlabel('log$_{10}(f)$')
+                ax[0].set_ylabel('log$_{10}|Z|$')
+                
+                ax[1].scatter(np.log10(df['Freq/Hz']), -df['Phase/deg'])
+                ax[1].set_xlabel('log$_{10}(f) $')
+                ax[1].set_ylabel('$-\phi$ / $\degree$')
+            else:
+                ax.plot(df['Zre/ohm'], -df['Zim/ohm'], label=labels[i])
+                ax.set_xlabel('$Z_{real}$ / $\Omega$')
+                ax.set_ylabel('$-Z_{imag}$ / $\Omega$')
+                
+        elif technique.lower() in ['cyclic voltammetry', 'linear sweep voltammetry', 'cv', 'lsv']:
+            ax.plot(df['Potential/V'], df['Current/A']*1000, label=labels[i])
+            ax.set_xlabel('$E$ vs. RE / V')
+            ax.set_ylabel('$i$ / mA')
+            
+        elif technique.lower() in ['ocp', 'ocpt', 'open circuit potential - time', 'multi-current steps', 'istep', 'cg']:
+            ax.plot(df['Time/sec'], df['Potential/V'], label=labels[i])
+            ax.set_ylabel('$t$ / s')
+            ax.set_ylabel('$E$ vs. RE / V')
+         
+    if draw_legend is True:
+        if order is not None and ax_isarray is False:
+            handles, labels = plt.gca().get_legend_handles_labels()
+            fig.legend([handles[idx] for idx in order],[labels[idx] for idx in order])
+        elif order is not None and ax_isarray is True:
+            raise TypeError('Arrays of axes cannot be reordered. Sorry :/')
+        else:
+            fig.legend()     
+         
+    return fig, ax
+
+def CHI_coplot_from_dict():
+    pass
