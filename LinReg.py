5ec28402542f9aa3db3c0442314a2778d6787f55
JGageWright <83935187+JGageWright@users.noreply.github.com>
2022-04-17 13:21:49 -0700

    Update LinReg

--- LinReg.py
+++ LinReg.py
@@ -1,9 +1,10 @@
 import numpy as np
+from numpy.linalg import lstsq
 import pandas as pd
 
 class PolyReg:
     """
-    No parameters are fixed.
+    No parameters can be fixed
     Object to hold all information associated with a polynomial model.
     Polynomial coefficients in order of decreasing degree are in coef[i].
     Note that ss_yy is commonly named ss_tot in other implementations.
@@ -30,6 +31,45 @@ class PolyReg:
         self.r_squared = 1 - (self.ss_res / self.ss_yy)
         self.std_err = np.sqrt(np.diag(self.cov))
         self.s_y = np.sqrt(self.ss_res / (len(ydata) - 1 - self.degree))
+        self.roots = np.roots(self.coef)
+        
+    def report(self):
+        '''
+        Returns some quantities of interest
+        '''
+        params = {}
+        for i in range(len(self.coef)):
+            params['coef_deg' + str(len(self.coef) - i -1)] = self.coef[i]
+            params['std_err_deg' + str(len(self.coef) - i -1)] = self.std_err[i]
+            
+        params['r_squared'] = self.r_squared
+        params['s_y'] = self.s_y
+        series = pd.Series(params)
+        return series
+    
+    def eval(self, x):
+        return np.polyval(self.coef, x)
+    
+    
+class LinFixB:
+    def __init__(self, xdata, ydata) -> None:
+        self.xdata = xdata
+        self.ydata = ydata
+        
+        x_col = xdata[:, np.newaxis] # x must be a column vector for np.lstsq
+        y_col = ydata[:, np.newaxis] # y must have same shape as x
+        
+        self.coef, ss_res, rank, s = lstsq(x_col, y_col, rcond=False)
+        b = np.zeros([1]) # insert intercept manually
+        self.coef = np.append(self.coef, b)
+        
+        self.cov = np.cov(xdata, ydata)
+        self.residuals = ydata - np.polyval(self.coef, xdata)
+        self.ss_res = np.sum(self.residuals**2)
+        self.ss_yy = np.sum((ydata) ** 2)
+        self.r_squared = 1 - (self.ss_res / self.ss_yy)
+        self.std_err = np.sqrt(np.diag(self.cov)) # Not sure if this is correct
+        self.s_y = np.sqrt(self.ss_res / (len(ydata) - 2)) # len(ydata) - 1 - degree of fit
         self.roots = np.roots(self.coef)
         
     def report(self):
